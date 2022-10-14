8521e380efc54e1d3b1fa882063f64775798cc29
Fabrizzio Lo Presti <fabrizziolopresti1999@gmail.com>
2022-10-13 22:38:25 -0300

    PUT /productos/:id

--- src/modules/productos/producto.controller.js
+++ src/modules/productos/producto.controller.js
@@ -4,9 +4,6 @@ const prisma = new PrismaClient();
 const getProductos = async (req, res) => {
   try {
     const data = await prisma.productos.findMany({
-      where: {
-        estado: true,
-      },
       include: {
         proveedores: true,
       },
@@ -14,7 +11,6 @@ const getProductos = async (req, res) => {
 
     const productos = []
     data.forEach(producto => {
-      // tomar por separdo nombre de producto y nombre de proveedor
       const { id, nombre, precio, stock, stockminimo, proveedores:{id: idProveedor, nombre:proveedor}, estado } = producto;
       productos.push({ id, nombre, precio, stock, stockminimo, idProveedor, proveedor, estado });
     });
@@ -67,7 +63,41 @@ const createProducto = async (req, res) => {
 }
 
 const updateProducto = async (req, res) => {
+  const { id } = req.params;
+  const { nombre, descripcion, precio, stock, stockminimo, idProveedor } = req.body;
+  try {
+    const productoFound = await prisma.productos.findUnique({
+      where: {
+        id: parseInt(id),
+      },
+    });
 
+    if (!productoFound) {
+      return res.status(400).json({ mensaje: 'El producto no existe', status: 400 });
+    }
+
+    const data = await prisma.productos.update({
+      where: {
+        id: parseInt(id),
+      },
+      data: {
+        nombre,
+        descripcion,
+        precio: parseFloat(precio),
+        stock: parseInt(stock),
+        stockminimo: parseInt(stockminimo),
+        estado: true,
+        proveedores: {
+          connect: {
+            id: parseInt(idProveedor),
+          }
+        }
+      },
+    });
+    res.status(200).json({ mensaje: 'Producto actualizado correctamente', status: 200 });
+  } catch (error) {
+    res.status(400).json({ mensaje: 'Error al actualizar producto', status: 400 });
+  }
 }
 
 const deleteProducto = async (req, res) => {
