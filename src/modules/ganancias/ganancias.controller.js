90852a6773ced875da5ce83e23904361f68c13b2
Fabrizzio Lo Presti <fabrizziolopresti1999@gmail.com>
2022-10-27 20:30:27 -0300

    POST /ganancias, UPDATE GETALL /ganancias

--- src/modules/ganancias/ganancias.controller.js
+++ src/modules/ganancias/ganancias.controller.js
@@ -1,4 +1,5 @@
 import { PrismaClient } from "@prisma/client";
+import jwt from 'jsonwebtoken';
 import { encryptPassword, matchPassword } from '../auth/auth.controller'
 const prisma = new PrismaClient();
 
@@ -8,15 +9,18 @@ const getGanancias = async (req, res) => {
       include: {
         usuarios: {
           select: {
-            usuario: true,
+            nombrecompleto: true,
           },
         }
-      }
+      },
+      orderBy: {
+        vigencia: 'desc',
+      },
     });
 
     const respuesta = data.map(ganancia => {
-      const { vigencia, porcentaje, idusuario: idUsuario, usuarios:{usuario} } = ganancia;
-      return { vigencia, porcentaje, idUsuario, usuario };
+      const { vigencia, porcentaje, idusuario: idUsuario, usuarios:{nombrecompleto} } = ganancia;
+      return { vigencia, porcentaje, idUsuario, usuario: nombrecompleto };
     });
 
     res.status(200).json( respuesta ); 
@@ -25,6 +29,48 @@ const getGanancias = async (req, res) => {
   }
 };
 
+const createGanancia = async (req, res) => {
+  const token = req.header('x-access-token');
+
+  if (!token) {
+    return res.status(401).json({ mensaje: 'No hay token, permiso no válido' });
+  }
+
+  const tokenDecoded = jwt.verify(token, process.env.SECRET);
+  const { id } = tokenDecoded;
+
+  try {
+    const { vigencia, porcentaje } = req.body;
+
+    const ultimaVigencia = await prisma.ganancias.findFirst({
+      orderBy: {
+        vigencia: 'desc',
+      },
+    });
+
+    const fechaUltimaVigencia = new Date(ultimaVigencia.vigencia);
+    const fechaVigencia = new Date(vigencia);
+    const diferencia = fechaVigencia - fechaUltimaVigencia;
+    const dias = Math.floor(diferencia / (1000 * 60 * 60 * 24));
+
+    if(dias <= 1) {
+      return res.status(400).json({ mensaje: 'La vigencia debe ser mayor a la última vigencia registrada por 1 día', status: 400 });
+    }
+
+    const data = await prisma.ganancias.create({
+      data: {
+        vigencia: new Date(vigencia),
+        porcentaje,
+        idusuario: id,
+      },
+    });
+    res.status(200).json({ mensaje: 'Ganancia creada correctamente', status: 200 });
+  } catch (error) {
+    res.status(400).json({ mensaje: 'Error al crear ganancia', status: 400 });
+  }
+}
+
 export {
   getGanancias,
+  createGanancia,
 }
