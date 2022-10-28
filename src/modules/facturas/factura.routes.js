d4fc6a0725798e4bb285035171ffb4c82f2e333e
JoaquinGimenez-112733 <101760750+JoaquinGimenez-112733@users.noreply.github.com>
2022-10-28 11:27:16 -0300

    DTO para PDF de facturas

--- src/modules/facturas/factura.routes.js
+++ src/modules/facturas/factura.routes.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { verifyToken, isAdmin } from '../../middlewares'
 import {
-  getFacturas
+  getFacturas,
+  getFactforPDF
 } from "./factura.controller";
 const router = Router();
 
 router.get("/", getFacturas);
+router.get("/pdf/:idFactura", getFactforPDF);
 
 export default router;
